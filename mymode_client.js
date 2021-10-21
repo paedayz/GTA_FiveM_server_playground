b7fcda3945cfc70bfa1ad21eb5803c38b33b4e0f
paedayz <pae16phasit@gmail.com>
2021-10-21 12:28:53 +0700

    [:rocket:]: register command

--- mymode_client.js
+++ mymode_client.js
@@ -19,3 +19,9 @@ on('onClientGameTypeStart', () => {
     exports.spawnmanager.setAutoSpawn(true)
     exports.spawnmanager.forceRespawn()
 })
+
+RegisterCommand('car', (source, args, raw) => {
+    emit('chat:addMessage', {
+        args: [`I wish I could spawn this ${args.length > 0 ? `${args[0]} or` : ``} adder but my owner was too lazy. :()`]
+    })
+}, false)
